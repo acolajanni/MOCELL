f6ec4c1be4948f670d5e0d7128be6776ecae0c21
acolajanni <73993119+acolajanni@users.noreply.github.com>
2021-09-16 18:44:33 +0200

    Update functions.R

--- functions.R
+++ functions.R
@@ -21,4 +21,4 @@ filter_GOI<- function(df, column_to_keep = "Gene", threshold = 1) {
 GOI_FUR_1[,c('Gene','bnum')]
 test=filter_GOI(GOI_FUR_1,c('Gene','bnum'))
 
-Regarder upset plot pour voir les intersections entre gene impliques méto fer / resistance / dypiridil / snRNA
+#Regarder upset plot pour voir les intersections entre gene impliques méto fer / resistance / dypiridil / snRNA
