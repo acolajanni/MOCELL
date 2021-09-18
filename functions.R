8ac20214bd531d1d6acbd4ea3957f86c8406dfa0
acolajanni <73993119+acolajanni@users.noreply.github.com>
2021-09-18 16:51:54 +0200

    Add files via upload

--- functions.R
+++ functions.R
@@ -14,12 +14,48 @@ filter_GOI<- function(df, column_to_keep = "Gene", threshold = 1) {
   # Using the log2 Fold change as the last column of the dataframe
   GOI = subset(df[column_to_keep], 
                vapply(X = df[,ncol(df)], abs, numeric(1)) >= threshold )
-  #Returning the gene inside a vector
-  return(as.vector(GOI))
+  #Returning the genes inside a vector
+  return(as.vector(unlist(GOI[[column_to_keep]])))
+
 }
 
-GOI_FUR_1[,c('Gene','bnum')]
-test=filter_GOI(GOI_FUR_1,c('Gene','bnum'))
+GOI_FUR_1 = read.delim("./GOI_FUR_1.txt")
+GOI_FUR_2 = read.delim("./GOI_FUR_2.txt")
+sRNA = read.delim("./sRNA_list_coli.txt")
 
-#Regarder upset plot pour voir les intersections entre gene impliques méto fer / resistance / dypiridil / snRNA
+
+GOI1=filter_GOI(GOI_FUR_1)
+GOI2=filter_GOI(GOI_FUR_2)
+GOI3=as.vector(unique(sRNA$sRNA))
+
+library(UpSetR)
+
+
+#Regarder upset plot pour voir les intersections entre gene impliques métabo fer / resistance / dypiridil / snRNA / !!!!!!!!!! Facteur de transcription !!!!!!!!!!
 # Fonction filtration coexpression : en reutilisant filter_GOI
+
+
+########## Pas besoin de fonction pour deux lignes de code ?
+GOI_list <- list(fur1 = GOI1, 
+                  fur2 = GOI2, 
+                  sRNA = GOI3)
+
+upset(fromList(GOI_list), 
+      sets.bar.color = "#56B4E9", 
+      order.by = "freq", 
+      text.scale = 1.5,
+      #mb.ratio = c(0.65,0.35),
+      empty.intersections = "on")
+
+
+
+#### Ou en une seule ligne :
+
+upset(fromList(list(fur1 = GOI1, 
+                    fur2 = GOI2, 
+                    sRNA = GOI3)), 
+      
+      sets.bar.color = "#56B4E9", 
+      order.by = "freq", 
+      text.scale = 1.5,
+      empty.intersections = "on")
